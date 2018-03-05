35bb4072228614057b0e182d65ec58267947a741
Tyler Maxwell <tmax818@mac.com>
2018-03-04 16:48:53 -0800

    initial commit.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, "secret"
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :index
+  end
+
 end
